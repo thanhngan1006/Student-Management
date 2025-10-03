c890ada0bdb5d6fba9a7ddb7682648195848204c
mon <thanhngan10604@gmail.com>
2025-10-04 00:45:47 +0700

    add middleware into user service

--- server/services/UserService/routes/authRoutes.js
+++ server/services/UserService/routes/authRoutes.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authControllers");
-const {authenticateToken} = require("../../../middleware/auth");
+// const {authenticateToken} = require("../../../middleware/auth");
+const { authenticateToken } = require("../middlewares/auth");
 
 router.get("/verify", authController.verify);
-router.post('/login', authController.login);
-router.post("/change-password", authenticateToken, authController.changePassword);
-router.post("/verify-password", authenticateToken, authController.verifyPassword);
-router.post('/send-reset-link', authController.sendResetLink);
-router.post('/reset-password', authController.resetPassword);
-
+router.post("/login", authController.login);
+router.post(
+  "/change-password",
+  authenticateToken,
+  authController.changePassword
+);
+router.post(
+  "/verify-password",
+  authenticateToken,
+  authController.verifyPassword
+);
+router.post("/send-reset-link", authController.sendResetLink);
+router.post("/reset-password", authController.resetPassword);
 
 module.exports = router;
--- server/services/UserService/routes/userRoutes.js
+++ server/services/UserService/routes/userRoutes.js
@@ -2,25 +2,66 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/usersControllers");
 const upload = require("../middlewares/upload");
-const { authenticateToken,  authorizeRoles, } = require("../../../middleware/auth");
+// const { authenticateToken,  authorizeRoles, } = require("../../../middleware/auth");
+const { authenticateToken, authorizeRoles } = require("../middlewares/auth");
 
 router.get("/advisors", userController.getAllAdvisors);
 router.post("/batch", userController.getUsersByIds);
 router.post("/teachers", userController.getTeacherByIds);
-router.put("/:id", authenticateToken,  authorizeRoles("admin", "advisor"),  userController.updateUserProfile);
+router.put(
+  "/:id",
+  authenticateToken,
+  authorizeRoles("admin", "advisor"),
+  userController.updateUserProfile
+);
 router.get("/:id", userController.getUserById);
-router.post("/import-file",  upload.single("file"),  authenticateToken,  authorizeRoles("admin"),  userController.importUsersFromFile); //admin import user
+router.post(
+  "/import-file",
+  upload.single("file"),
+  authenticateToken,
+  authorizeRoles("admin"),
+  userController.importUsersFromFile
+); //admin import user
 router.post("/get-ids-by-emails", userController.getUserIdsByEmails); //lay id của user thông qua email
-router.post("/import-advisors",  upload.single("file"), authenticateToken,  authorizeRoles("admin"),  userController.importAdvisors); //admin import giáo viên
+router.post(
+  "/import-advisors",
+  upload.single("file"),
+  authenticateToken,
+  authorizeRoles("admin"),
+  userController.importAdvisors
+); //admin import giáo viên
 router.get("/tdt/:tdt_id", userController.getUserByTdtId);
-router.delete("/:id",  authenticateToken,  authorizeRoles("admin"),  userController.deleteAdvisor);
-router.post("/add-student",  authenticateToken,  authorizeRoles("admin"),  userController.addStudentByAdmin);
-router.post("/add-advisor",  authenticateToken,  authorizeRoles("admin"),  userController.addAdvisorByAdmin);
+router.delete(
+  "/:id",
+  authenticateToken,
+  authorizeRoles("admin"),
+  userController.deleteAdvisor
+);
+router.post(
+  "/add-student",
+  authenticateToken,
+  authorizeRoles("admin"),
+  userController.addStudentByAdmin
+);
+router.post(
+  "/add-advisor",
+  authenticateToken,
+  authorizeRoles("admin"),
+  userController.addAdvisorByAdmin
+);
 router.get("/", userController.getAllStudents);
-router.delete("/full-delete/:id",  authenticateToken, authorizeRoles("admin"),  userController.fullDeleteStudent);
-router.put('/:id/add-homeroom-teacher', userController.addHomeroomTeacher);
-router.put('/:id/remove-homeroom-teacher', userController.removeHomeroomTeacher);
-router.post('/graduated', userController.graduated);
-router.post('/:id/repeat', userController.addRepeatYear);
+router.delete(
+  "/full-delete/:id",
+  authenticateToken,
+  authorizeRoles("admin"),
+  userController.fullDeleteStudent
+);
+router.put("/:id/add-homeroom-teacher", userController.addHomeroomTeacher);
+router.put(
+  "/:id/remove-homeroom-teacher",
+  userController.removeHomeroomTeacher
+);
+router.post("/graduated", userController.graduated);
+router.post("/:id/repeat", userController.addRepeatYear);
 
 module.exports = router;
