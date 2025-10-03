998daf630a71fae8270007d2139efd1248a2b731
mon <thanhngan10604@gmail.com>
2025-10-04 00:42:52 +0700

    add middleware into Education service

--- server/services/EducationService/routes/semesterRoutes.js
+++ server/services/EducationService/routes/semesterRoutes.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const semesterController = require("../controllers/semesterControllers");
-const upload = require('../middlewares/upload');
-const { authenticateToken, authorizeRoles } = require("../../../middleware/auth");
+const upload = require("../middlewares/upload");
+// const { authenticateToken, authorizeRoles } = require("../../../middleware/auth");
+const { authenticateToken, authorizeRoles } = require("../middlewares/auth");
 
-router.get('/school-years', semesterController.getSchoolYear);
-router.get('/semester-by-schoolyear', semesterController.getSemesterBySchoolYear);
+router.get("/school-years", semesterController.getSchoolYear);
+router.get(
+  "/semester-by-schoolyear",
+  semesterController.getSemesterBySchoolYear
+);
 
-router.get('/current', semesterController.getCurrentSemester);
-router.get('/:id', semesterController.getSemesterById);
-router.get('/', semesterController.getAllSemesters);
-router.post('/import-semesters', upload.single('file'), authenticateToken, authorizeRoles('admin', 'advisor'), semesterController.importSemesters);
-router.get('/code/:code', semesterController.getSemesterByCode);
-router.post('/by-years', semesterController.getSemestersByYears);
+router.get("/current", semesterController.getCurrentSemester);
+router.get("/:id", semesterController.getSemesterById);
+router.get("/", semesterController.getAllSemesters);
+router.post(
+  "/import-semesters",
+  upload.single("file"),
+  authenticateToken,
+  authorizeRoles("admin", "advisor"),
+  semesterController.importSemesters
+);
+router.get("/code/:code", semesterController.getSemesterByCode);
+router.post("/by-years", semesterController.getSemestersByYears);
 
 module.exports = router;
--- server/services/EducationService/routes/subjectRoutes.js
+++ server/services/EducationService/routes/subjectRoutes.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const subjectController = require("../controllers/subjectControllers");
-const upload = require('../middlewares/upload');
-const { authenticateToken, authorizeRoles } = require("../../../middleware/auth");
+const upload = require("../middlewares/upload");
+// const { authenticateToken, authorizeRoles } = require("../../../middleware/auth");
+const { authenticateToken, authorizeRoles } = require("../middlewares/auth");
 
-router.get('/', subjectController.getAllSubjects);
-router.post('/batch', subjectController.getSubjectsByIds);
-router.post('/import-subjects', upload.single('file'), authenticateToken, authorizeRoles('admin', 'advisor'), subjectController.importSubjects);
-router.get('/code/:code', subjectController.getSubjectByCode);
-router.get('/:id', subjectController.getSubjectById); 
+router.get("/", subjectController.getAllSubjects);
+router.post("/batch", subjectController.getSubjectsByIds);
+router.post(
+  "/import-subjects",
+  upload.single("file"),
+  authenticateToken,
+  authorizeRoles("admin", "advisor"),
+  subjectController.importSubjects
+);
+router.get("/code/:code", subjectController.getSubjectByCode);
+router.get("/:id", subjectController.getSubjectById);
 
 module.exports = router;
