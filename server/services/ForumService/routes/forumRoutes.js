df1d4f9b7094d2320bf997ed985fdbd36f9aa438
mon <thanhngan10604@gmail.com>
2025-10-04 00:39:40 +0700

    add middleware into ForumService

--- server/services/ForumService/routes/forumRoutes.js
+++ server/services/ForumService/routes/forumRoutes.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const forumController = require('../controllers/forumControllers');
-const { authenticateToken, allowStudentAndAdvisorOnlyAndOwner } = require("../../../middleware/auth");
+const forumController = require("../controllers/forumControllers");
+// const { authenticateToken, allowStudentAndAdvisorOnlyAndOwner } = require("../../../middleware/auth");
+const {
+  authenticateToken,
+  allowStudentAndAdvisorOnlyAndOwner,
+} = require("../middlewares/auth");
 
 router.get("/class/:classId/posts", forumController.getPostsByClass);
-router.post("/posts", authenticateToken, allowStudentAndAdvisorOnlyAndOwner, forumController.createPost);
-router.post("/posts/:postId/comments", authenticateToken, forumController.addComment);
+router.post(
+  "/posts",
+  authenticateToken,
+  allowStudentAndAdvisorOnlyAndOwner,
+  forumController.createPost
+);
+router.post(
+  "/posts/:postId/comments",
+  authenticateToken,
+  forumController.addComment
+);
 router.post("/posts/:postId/like", forumController.toggleLike);
 router.delete("/posts/:postId", authenticateToken, forumController.deletePost);
 
-module.exports = router; 
+module.exports = router;
